7567bc7c82deb8a01fadf4fb1051cf7b26caa349
Rektable <144263540+Rektable@users.noreply.github.com>
2023-09-06 20:50:49 +0300

    Add files via upload

---
+++ Hello World Try 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System; // Это чтобы не писать по сто раз, юзаю как shortcut
+
+namespace HelloWorld // Здесь это название проги
+{
+    class Hello // Без задачи класса нельзя
+    {
+        static void Main(string[] args) // Начало проги всегда с main
+        {
+            Console.WriteLine("Озон терпел и нам велел"); // Консоль оси открывается и выдает текст
+        }
+    }
+}
