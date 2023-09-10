76b990185d7f8df61b9492824b0237766e476692
Jake Hawkfield <144263540+Rektable@users.noreply.github.com>
2023-09-10 14:46:42 +0300

    Sum of Natural Numbers
    Я ненавижу войды.

---
+++ CSharp_algorithmization_task1_Algorithm4/CSharp_algorithmization_task1_Algorithm4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program // база
+{
+    static void Main(string[] args) // база
+    {
+
+        Console.Write("Введите N: ");
+        int N = int.Parse(Console.ReadLine()); // запись числа
+
+
+        int sum = 0;
+        for (int i = 1; i <= N; i++) // формула шага цикла
+        {
+            sum += i; // цикл i добавляется для считывания суммы
+        }
+
+        Console.WriteLine("Сумма первых {0} натуральных чисел, при шаге цикла единица: {1}", N, sum); // Вывод
+    }
+}
+
+
+
