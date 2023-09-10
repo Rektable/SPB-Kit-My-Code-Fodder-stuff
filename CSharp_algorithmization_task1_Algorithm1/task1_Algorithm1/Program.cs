5f2bf187dd430d6831d1a09d946a4283b0d6cd72
Jake Hawkfield <144263540+Rektable@users.noreply.github.com>
2023-09-10 13:14:01 +0300

    Add files via upload

---
+++ CSharp_algorithmization_task1_Algorithm1/task1_Algorithm1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System; // пространство времен под названием system не будет конфликтовать с другими, а именно обьявление имен или переменных.
+
+class Algorithm1_lowestnumber // без задачи класса программа не начнётся на C#
+{
+
+    static void Main(string[] args) // начало программы
+    {
+        {
+            Console.Write("Введите первое число: ");
+            int number1 = Convert.ToInt32(Console.ReadLine()); // задаем первое число, конвертируется в 32бита число чтобы распознать
+            Console.Write("Введите второе число: ");
+            int number2 = Convert.ToInt32(Console.ReadLine()); // второе число
+            Console.Write("Введите третье число: ");
+            int number3 = Convert.ToInt32(Console.ReadLine()); // третье число
+            
+            int min_value = Math.Min(Math.Min(number1, number2), number3); // Минимальное значение
+
+            
+            Console.WriteLine("Минимальное число: " // Выдаем результат
+                              + min_value);
+        }
+    }
+}
