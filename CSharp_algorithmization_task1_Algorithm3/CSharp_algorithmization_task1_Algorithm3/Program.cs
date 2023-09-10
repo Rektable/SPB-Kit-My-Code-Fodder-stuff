245112ac95900eb58959d3dcccb1d216f80fb3c6
Jake Hawkfield <144263540+Rektable@users.noreply.github.com>
2023-09-10 14:00:45 +0300

    Factorial
    Расчёт факториала, лучше брать числа до 50. Выше не тестировал и не хочу.

---
+++ CSharp_algorithmization_task1_Algorithm3/CSharp_algorithmization_task1_Algorithm3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Numerics;
+
+
+namespace Algorithm3_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args) // без этого никак
+        {
+            Console.WriteLine("Введите число");
+            long n = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine(fact(n));
+            Console.ReadLine();
+        }
+        static BigInteger fact(BigInteger num) // библиотекой System.Numerics вычисляет факториал. Большие числа лучше не вставлять, чревато. Будет долго, а при огромных зависнет.
+        {
+            return (num == 0) ? 1 : num * fact(num - 1); // сама формула расчёта факториала
+        }
+
+    }
+}
